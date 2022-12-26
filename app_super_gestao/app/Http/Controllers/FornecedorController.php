ede674bb1b0ce845923f799e54b4ef4ba139486c
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2022-12-26 13:09:45 -0300

    Aula 48
    Blade - @empty

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -6,17 +6,18 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $fornecedores = [
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00.000.000/000-00'
+                'cnpj' => '00'
             ],
             1 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N'
-            ], 
+            ],
         ];
 
         return view('app.fornecedor.index', compact('fornecedores'));
