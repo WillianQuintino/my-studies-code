5fe5ff3473828806b2cc061d64f6e19a2182042c
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2022-12-26 13:17:03 -0300

    Aula 49
    Extra - Operador condicional ternário

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -17,9 +17,16 @@ class FornecedorController extends Controller
             1 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N'
-            ],
+            ]
         ];
-
+        
+        /*
+        condica ? se verdadeiro : se falso;
+        condica ? se verdadeiro : (condica ? se verdadeiro : se falso;);
+        */
+        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
+        echo $msg;
+        
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
