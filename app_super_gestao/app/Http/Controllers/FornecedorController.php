69bf64b6fbc8d9a81b41fa46888ce1cb43c885ac
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2022-12-26 18:45:33 -0300

    Aula 53
    Blade - @while

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -31,13 +31,7 @@ class FornecedorController extends Controller
                 'telefone' => '0000-0000'
             ]
         ];
-        
-        /*
-        condica ? se verdadeiro : se falso;
-        condica ? se verdadeiro : (condica ? se verdadeiro : se falso;);
-        */
-        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
-        echo $msg;        
+             
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
