4028c188a3d5314bdd637317397581d0e6cf0629
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2022-12-27 09:39:50 -0300

    Aula 65
    Super Gestão - enviando o formulário de contato

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Facade\Ignition\DumpRecorder\Dump;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
     public function contato()
     {
+        var_dump($_GET);
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
