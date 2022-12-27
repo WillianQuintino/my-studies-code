c9c2bc6c4685ae5fc9fd60ef18d10c238a99cc4b
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2022-12-27 11:16:23 -0300

    Aula 69
    Blade - Enviando parâmetros para componentes

--- app_super_gestao/resources/views/site/contato.blade.php
+++ app_super_gestao/resources/views/site/contato.blade.php
@@ -10,7 +10,9 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                @component('site.layouts._components.form_contato')
+                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                    <p>A nossa equipe analizará a sua mensagem e retortnamermos o mais brevemente possível.</p>
+                    <p>Nosso tempo médio de resposta é de 48 horas.</p>
                 @endcomponent
             </div>
         </div>
