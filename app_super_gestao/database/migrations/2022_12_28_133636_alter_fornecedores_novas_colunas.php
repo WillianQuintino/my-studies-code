771bd2b5ffbbcf77a98dbb8658e8419386f57528
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2022-12-28 10:52:08 -0300

    Aula 82
    Migration - Métodos up e down

--- app_super_gestao/database/migrations/2022_12_28_133636_alter_fornecedores_novas_colunas.php
+++ app_super_gestao/database/migrations/2022_12_28_133636_alter_fornecedores_novas_colunas.php
@@ -12,8 +12,8 @@ class AlterFornecedoresNovasColunas extends Migration
      * @return void
      */
     public function up()
-    {   Schema::table('fornecedores', function (Blueprint $table) {
-            $table->string('nome', 50);
+    {   
+        Schema::table('fornecedores', function (Blueprint $table) {
             $table->string('uf', 2);
             $table->string('email', 150);
         });     
@@ -27,6 +27,10 @@ class AlterFornecedoresNovasColunas extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('fornecedores', function (Blueprint $table) {
+            //para remover colunas
+            //$table->dropColumn('uf');
+            $table->dropColumn(['nome', 'uf', 'email']);
+        });  
     }
 }
