bda69288356928fef7d7d5fa3af93ad067ddada9
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2022-12-29 08:36:52 -0300

    Aula 92
    Eloquent- Ajustando o nome da tabela no Model para m correto ORM

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -4,7 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//fornecedors
+//fornecedores
+
 class Fornecedor extends Model
 {
     //
+    protected $table = 'fornecedores';
 }
--- app_super_gestao/app/SiteContato.php
+++ app_super_gestao/app/SiteContato.php
@@ -4,6 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//Site_Contato
+//site_contato
+//Site_Contatos
+
 class SiteContato extends Model
 {
     //
