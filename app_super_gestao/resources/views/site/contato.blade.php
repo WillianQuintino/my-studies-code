6aca5302af6795293f25f959a9094c3c07f8c54b
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2023-01-10 10:26:53 -0300

    Aula 125
    Repopulando o formulário (Request Old Input) parte 2

--- app_super_gestao/resources/views/site/contato.blade.php
+++ app_super_gestao/resources/views/site/contato.blade.php
@@ -10,14 +10,14 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta', 'motivo_contatos' => $motivo_contatos])
                     <p>A nossa equipe analizará a sua mensagem e retortnamermos o mais brevemente possível.</p>
                     <p>Nosso tempo médio de resposta é de 48 horas.</p>
                 @endcomponent
             </div>
         </div>
     </div>
-
+    
     <div class="rodape">
         <div class="redes-sociais">
             <h2>Redes sociais</h2>
