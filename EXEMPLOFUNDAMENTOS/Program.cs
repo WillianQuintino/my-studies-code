5565bfa10cac2ef20acf294a6deadbcb4d25c0c9
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2023-01-17 19:43:26 -0300

    Ordem dos operadores
    Tipos de Operadores em C#
    - Operadores de Atribuição
    -- Ordem dos operadores

--- EXEMPLOFUNDAMENTOS/Program.cs
+++ EXEMPLOFUNDAMENTOS/Program.cs
@@ -2,16 +2,33 @@
 
 
 
+double a = 4 / (2 + 2);
+
+Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // int a = 5;
 // double b = a;
 
 // long a = long.MaxValue;
 // int b = Convert.ToInt32(a);
 
-int a = int.MaxValue;
-long b = a;
+// int a = int.MaxValue;
+// long b = a;
 
-Console.WriteLine(b);
+// Console.WriteLine(b);
 
 
 
