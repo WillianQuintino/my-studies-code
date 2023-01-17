7586c27db7c48d77ad74b325c4ce3302cd404f40
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2023-01-17 18:40:49 -0300

    Declarando os tipos de variáveis
    Sintaxe e Tipos de Dados em C#
    - Tipos de dados
    -- Declarando os tipos de variáveis

--- EXEMPLOFUNDAMENTOS/Program.cs
+++ EXEMPLOFUNDAMENTOS/Program.cs
@@ -1,10 +1,29 @@
 ﻿using EXEMPLOFUNDAMENTOS.Models;
 
-Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome = "buta";
-pessoa1.Idade =26;
+string apresentacao = "Olá, seja bem vindo";
 
-pessoa1.Apresentar(); 
+int quantidade = 1;
 
-Pessoa _pessoa_FisicaRepresentacao = new Pessoa();
+double altura = 1.00;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade:" + quantidade);
+Console.WriteLine("Valor da variável altura:" + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preco:" + preco);
+Console.WriteLine("Valor da variável condicao:" + condicao);
+
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "buta";
+//pessoa1.Idade =26;
+
+//pessoa1.Apresentar(); 
+
+//Pessoa _pessoa_FisicaRepresentacao = new Pessoa();
