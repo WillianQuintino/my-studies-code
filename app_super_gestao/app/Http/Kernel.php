eeb305fd04609ef4334f9a3a73b6fd7a02084baa
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2023-01-20 09:31:34 -0300

    Aula 140
    Apelidando middlewares

--- app_super_gestao/app/Http/Kernel.php
+++ app_super_gestao/app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\LogAcessoMiddleware::class,
     ];
 
     /**
@@ -37,7 +36,8 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,            
+            // \App\Http\Middleware\LogAcessoMiddleware::class,
         ],
 
         'api' => [
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'log.acesso' => \App\Http\Middleware\LogAcessoMiddleware::class,
     ];
 }
