9fea06f5070c10b6e1e1349b190c95156324f053
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2023-01-20 11:43:04 -0300

    Aula 143
    Passando parâmetros para o middleware

--- app_super_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
+++ app_super_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
@@ -14,9 +14,25 @@ class AutenticacaoMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $metodo_atenticacao, $perfil, $param3, $param4)
     {
         //verifica se o usuario tem acesso a rota
+        echo $metodo_atenticacao.' - '.$perfil.'<br>';
+
+        if ($metodo_atenticacao == 'padrao') {
+            echo 'Verificar o usuário e senha no banco de dados'.$perfil.' <br>';
+        } 
+
+        if ($metodo_atenticacao == 'ldap') {
+            echo 'Verificar o usuário e senha no AD'.$perfil.'<br>';
+        }
+        
+        if ($perfil == 'visitante') {
+            echo 'Exibir apenas alguns recursos';
+        }else{
+            echo 'Carregar o perfil do banco de dados';
+        }
+
         if(false) {
             return $next($request);
         }else{            
