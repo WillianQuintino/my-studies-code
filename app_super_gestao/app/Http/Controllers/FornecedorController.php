24e91ffa1ea2b75dd8e8ff9b55811314792eaf37
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2023-02-02 11:29:58 -0300

    Aula 158
    Implementando o cadastro de fornecedores parte 5 (remoção de registros)

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -79,4 +79,12 @@ class FornecedorController extends Controller
 
         return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]);
     }
+
+    public function excluir($id)
+    {
+        $fornecedor = Fornecedor::find($id)->delete();
+        //$fornecedor = Fornecedor::find($id)->forceDelete();
+
+        return redirect()->route('app.fornecedor');
+    }
 }
