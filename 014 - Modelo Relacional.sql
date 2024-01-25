b7d2d2832744053906cf68ab771948a11afe6716
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2024-01-25 12:49:25 -0300

    Create 014 - Modelo Relacional.sql

---
+++ 014 - Modelo Relacional.sql
@@ -0,0 +1,22 @@
+#entidade contem dados agrupados = tabela
+#dado as colunas = atributos
+
+# primeito ponto do modelo relacional é necessario o uso de chaves primarias
+
+# DER - DIAGRAMA ENTIDADE RELACIONAMENTO
+
+# carnilade simples e multiplas
+
+# existe conexão de N para N = Multiplos para multiplos
+
+#chave primaria, nao repete
+
+#chave estrageira é a chave é de relacionamento que vem de outra tabela.
+
+#relacionamento 
+# 1 para 1 - coloca a chave extrageira no lado dominate
+# 1 para n - coloca a chave estrageira no lado N
+# n para n - cria uma nova tabela
+	# nesta nova tabela ela tem um id, e outros atributos
+    # traformando a licação em 1 para n para 1 
+    # logo a chave estrageira vai para n a tabela nova
