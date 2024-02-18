28a4cc2fb76d7bf7492d17684fe6a47e01b93c5c
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2024-02-18 03:41:57 +0000

    atualização do codigo

---
+++ jogos-digitais/1o-semestre/programacao-ii/ex001.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+
+    setlocale(LC_CTYPE, "portuguese");// Configuração acento
+
+    //declaração de Variável
+    int num;
+    
+    // Dados de Entrada
+    printf ("Digite um número: ");
+    scanf ("%i", &num);
+
+    for (int linha = 1; linha < num; linha++)
+    {
+        for (int ast = 0; ast < linha; ast++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+    
+
+    return 0;
+}
